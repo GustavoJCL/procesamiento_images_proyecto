2fd8a63d50cbec8829f00c4c731ac8ade24b1632
GustavoJCL <gjcalizayal@unjbg.edu.com>
2023-09-21 13:46:42 -0500

    fix of bugs and comment 2 options

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,22 +5,30 @@ use base64::{decode, encode};
 use image::{DynamicImage, GenericImageView, ImageBuffer};
 use imageproc::distance_transform::Norm;
 use imageproc::morphology::{dilate, erode};
+use photon_rs::monochrome::grayscale;
 use photon_rs::{channels, conv, effects, PhotonImage};
 use rand::Rng;
 
 // #[derive(Serialize, Deserialize)]
 #[tauri::command(rename_all = "snake_case")]
-fn image_enhance(image_data_base64: String, amt: i16) -> Result<String, String> {
-    let bytes = decode(&image_data_base64).map_err(|e| e.to_string())?;
-    let mut img = PhotonImage::new_from_byteslice(bytes);
+fn image_enhance(image_data_base64: &str, amt: i16) -> Result<String, String> {
+    // let bytes = decode(&image_data_base64).map_err(|e| e.to_string())?;
+    let mut img = PhotonImage::new_from_base64(&image_data_base64);
 
     // Apply enhancements
     channels::alter_red_channel(&mut img, amt);
+    effects::solarize(&mut img);
 
     // Convert the enhanced image back to bytes
-    let raw_pixels = img.get_raw_pixels();
-    let enhanced_image = encode(&raw_pixels);
-    Ok(enhanced_image)
+    // let mut buffer: Vec<u8> = Vec::new();
+    // for pixel in img.get_raw_pixels() {
+    //     buffer.push(pixel);
+    // }
+    // let raw_pixels = img.get_raw_pixels();
+    // let enhanced_image = encode(&buffer);
+    // enhanced_image
+    // Ok(enhanced_image)
+    Ok(img.get_base64())
 }
 
 #[tauri::command(rename_all = "snake_case")]
@@ -29,18 +37,20 @@ fn restore_image(
     brightness: u8,
     contrast: f32,
 ) -> Result<String, String> {
-    let bytes = decode(&image_data_base64).map_err(|e| e.to_string())?;
-    let mut img = PhotonImage::new_from_byteslice(bytes);
+    // let bytes = decode(&image_data_base64).map_err(|e| e.to_string())?;
+    let mut img = PhotonImage::new_from_base64(&image_data_base64);
 
     // Apply restoration
     effects::inc_brightness(&mut img, brightness);
     effects::adjust_contrast(&mut img, contrast);
+    grayscale(&mut img);
 
     // Convert the restored image back to bytes
 
-    let raw_pixels = img.get_raw_pixels();
-    let restored_image = encode(&raw_pixels);
-    Ok(restored_image)
+    // let raw_pixels = img.get_raw_pixels();
+    // let restored_image = encode(&raw_pixels);
+    // Ok(restored_image)
+    Ok(img.get_base64())
 }
 
 #[tauri::command(rename_all = "snake_case")]
@@ -78,8 +88,9 @@ fn denoising_image_gausian_blur(image_data_base64: String, radius: i32) -> Resul
     // Denoising the image
     conv::gaussian_blur(&mut img, radius);
 
-    let denoised_image = encode(img.get_raw_pixels());
-    Ok(denoised_image)
+    // let denoised_image = encode(img.get_raw_pixels());
+    // Ok(denoised_image)
+    Ok(img.get_base64())
 }
 #[tauri::command(rename_all = "snake_case")]
 fn denoising_image_nlm(
@@ -144,114 +155,138 @@ fn non_local_means(img: &DynamicImage, window_size: i32, h: f64) -> DynamicImage
     DynamicImage::ImageRgb8(denoised_img).to_owned()
 }
 
-fn kmeans(data: &Vec<(f64, f64, f64)>, k: usize) -> (Vec<(f64, f64, f64)>, Vec<usize>) {
-    // Initialize cluster centroids randomly
-    let mut rgn = rand::thread_rng();
-    let mut centroids: Vec<(f64, f64, f64)> = Vec::new();
-    for _ in 0..k {
-        let index = rgn.gen_range(0..data.len());
-        centroids.push(data[index]);
-    }
+// fn kmeans(data: &Vec<(f64, f64, f64)>, k: usize) -> (Vec<(f64, f64, f64)>, Vec<usize>) {
+//     // Initialize cluster centroids randomly
+//     let mut rgn = rand::thread_rng();
+//     let mut centroids: Vec<(f64, f64, f64)> = Vec::new();
+//     for _ in 0..k {
+//         let index = rgn.gen_range(0..data.len());
+//         centroids.push(data[index]);
+//     }
+//
+//     // Initialize cluster assignments
+//     let mut assignments: Vec<usize> = vec![0; data.len()];
+//
+//     loop {
+//         // Assign data points to the nearest centroid
+//         let mut updated_assignments = false;
+//         for (i, point) in data.iter().enumerate() {
+//             let mut min_distance = f64::MAX;
+//             let mut min_index = 0;
+//             for (j, centroid) in centroids.iter().enumerate() {
+//                 let distance = euclidean_distance(point, centroid);
+//                 if distance < min_distance {
+//                     min_distance = distance;
+//                     min_index = j;
+//                 }
+//             }
+//             if assignments[i] != min_index {
+//                 assignments[i] = min_index;
+//                 updated_assignments = true;
+//             }
+//         }
+//
+//         if !updated_assignments {
+//             break;
+//         }
+//
+//         // Update centroids
+//         let mut cluster_sums: Vec<(f64, f64, f64)> = vec![(0.0, 0.0, 0.0); k];
+//         let mut cluster_counts: Vec<usize> = vec![0; k];
+//         for (i, point) in data.iter().enumerate() {
+//             let cluster_index = assignments[i];
+//             let (sum_x, sum_y, sum_z) = cluster_sums[cluster_index];
+//             cluster_sums[cluster_index] = (sum_x + point.0, sum_y + point.1, sum_z + point.2);
+//             cluster_counts[cluster_index] += 1;
+//         }
+//         for j in 0..k {
+//             let count = cluster_counts[j];
+//             if count > 0 {
+//                 let centroid = (
+//                     cluster_sums[j].0 / count as f64,
+//                     cluster_sums[j].1 / count as f64,
+//                     cluster_sums[j].2 / count as f64,
+//                 );
+//                 centroids[j] = centroid;
+//             }
+//         }
+//     }
+//
+//     (centroids, assignments)
+// }
+//
+// fn euclidean_distance(p1: &(f64, f64, f64), p2: &(f64, f64, f64)) -> f64 {
+//     let dx = p1.0 - p2.0;
+//     let dy = p1.1 - p2.1;
+//     let dz = p1.2 - p2.2;
+//     (dx * dx + dy * dy + dz * dz).sqrt()
+// }
+//
+// fn recolor_image(
+//     data: &Vec<(f64, f64, f64)>,
+//     results: &(Vec<(f64, f64, f64)>, Vec<usize>),
+//     img: &image::RgbImage,
+// ) -> image::RgbImage {
+//     let (centroids, assignments) = results;
+//     let mut new_img: image::RgbImage = ImageBuffer::new(img.width(), img.height());
+//     for (i, (x, y, pixel)) in data.iter().enumerate() {
+//         let cluster = assignments[i];
+//         let (r, g, b) = centroids[cluster];
+//         new_img.put_pixel(
+//             *x as u32,
+//             *y as u32,
+//             image::Rgb([r as u8, g as u8, b as u8]),
+//         );
+//     }
+//     new_img
+// }
 
-    // Initialize cluster assignments
-    let mut assignments: Vec<usize> = vec![0; data.len()];
+// #[tauri::command(rename_all = "snake_case")]
+// fn segment_image_kmeans(image_data_base64: String, k: usize) -> Result<String, String> {
+//     let image_bytes = decode(image_data_base64).map_err(|e| e.to_string())?;
+//     // let img_2 = image::open("input.png").unwrap().into_rgb8();
+//     let img = image::load_from_memory(&image_bytes).unwrap().into_rgb8();
+//
+//     let vec_img = img
+//         .pixels()
+//         .map(|pixel| {
+//             (
+//                 pixel[0] as f64 / 255.0,
+//                 pixel[1] as f64 / 255.0,
+//                 pixel[2] as f64 / 255.0,
+//             )
+//         })
+//         .collect::<Vec<_>>();
+//     let results_img = kmeans(&vec_img, k);
+//     let new_img = recolor_image(&vec_img, &results_img, &img);
+//
+//     Ok(encode(new_img.as_raw()))
+// }
 
-    loop {
-        // Assign data points to the nearest centroid
-        let mut updated_assignments = false;
-        for (i, point) in data.iter().enumerate() {
-            let mut min_distance = f64::MAX;
-            let mut min_index = 0;
-            for (j, centroid) in centroids.iter().enumerate() {
-                let distance = euclidean_distance(point, centroid);
-                if distance < min_distance {
-                    min_distance = distance;
-                    min_index = j;
-                }
+fn segmenta(x: &PhotonImage, t: u8) -> PhotonImage {
+    let (width, height) = (x.get_width(), x.get_height());
+    // let img_vec = Vec::<u8>::new();
+    let x_pixels = x.get_raw_pixels();
+    let mut y_pixels = x_pixels.clone();
+
+    for i in 0..width {
+        for j in 0..height {
+            let pixel = x_pixels[(i * height + j) as usize];
+            if pixel > t {
+                y_pixels[(i * height + j) as usize] = 255
             }
-            if assignments[i] != min_index {
-                assignments[i] = min_index;
-                updated_assignments = true;
-            }
-        }
-
-        if !updated_assignments {
-            break;
-        }
-
-        // Update centroids
-        let mut cluster_sums: Vec<(f64, f64, f64)> = vec![(0.0, 0.0, 0.0); k];
-        let mut cluster_counts: Vec<usize> = vec![0; k];
-        for (i, point) in data.iter().enumerate() {
-            let cluster_index = assignments[i];
-            let (sum_x, sum_y, sum_z) = cluster_sums[cluster_index];
-            cluster_sums[cluster_index] = (sum_x + point.0, sum_y + point.1, sum_z + point.2);
-            cluster_counts[cluster_index] += 1;
-        }
-        for j in 0..k {
-            let count = cluster_counts[j];
-            if count > 0 {
-                let centroid = (
-                    cluster_sums[j].0 / count as f64,
-                    cluster_sums[j].1 / count as f64,
-                    cluster_sums[j].2 / count as f64,
-                );
-                centroids[j] = centroid;
-            }
         }
     }
-
-    (centroids, assignments)
-}
-
-fn euclidean_distance(p1: &(f64, f64, f64), p2: &(f64, f64, f64)) -> f64 {
-    let dx = p1.0 - p2.0;
-    let dy = p1.1 - p2.1;
-    let dz = p1.2 - p2.2;
-    (dx * dx + dy * dy + dz * dz).sqrt()
-}
-
-fn recolor_image(
-    data: &Vec<(f64, f64, f64)>,
-    results: &(Vec<(f64, f64, f64)>, Vec<usize>),
-    img: &image::RgbImage,
-) -> image::RgbImage {
-    let (centroids, assignments) = results;
-    let mut new_img: image::RgbImage = ImageBuffer::new(img.width(), img.height());
-    for (i, (x, y, pixel)) in data.iter().enumerate() {
-        let cluster = assignments[i];
-        let (r, g, b) = centroids[cluster];
-        new_img.put_pixel(
-            *x as u32,
-            *y as u32,
-            image::Rgb([r as u8, g as u8, b as u8]),
-        );
-    }
-    new_img
+    let y = PhotonImage::new(y_pixels, width, height);
+    y
 }
 
 #[tauri::command(rename_all = "snake_case")]
-fn segment_image_kmeans(image_data_base64: String, k: usize) -> Result<String, String> {
-    let image_bytes = decode(image_data_base64).map_err(|e| e.to_string())?;
-    // let img_2 = image::open("input.png").unwrap().into_rgb8();
-    let img = image::load_from_memory(&image_bytes).unwrap().into_rgb8();
-
-    let vec_img = img
-        .pixels()
-        .map(|pixel| {
-            (
-                pixel[0] as f64 / 255.0,
-                pixel[1] as f64 / 255.0,
-                pixel[2] as f64 / 255.0,
-            )
-        })
-        .collect::<Vec<_>>();
-    let results_img = kmeans(&vec_img, k);
-    let new_img = recolor_image(&vec_img, &results_img, &img);
-
-    Ok(encode(new_img.as_raw()))
+fn segment_image(image_data_base64: String, threshold: u8) -> Result<String, String> {
+    let img = PhotonImage::new_from_base64(&image_data_base64);
+    let new_img = segmenta(&img, threshold);
+    Ok(new_img.get_base64())
 }
-
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
@@ -261,7 +296,7 @@ fn main() {
             morphological_dilation,
             denoising_image_gausian_blur,
             denoising_image_nlm,
-            segment_image_kmeans
+            segment_image
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,309 @@
+// import { resolve } from "@tauri-apps/api/path";
 import { invoke } from "@tauri-apps/api/tauri";
+
+enum ButtonOptions {
+	imageEnhance = "image-enhance",
+	imageRestoration = "image-restoration",
+	morphologicalErosion = "morphological-erosion",
+	morphologicalDilation = "morphological-dilation",
+	denoisingGausian = "denoising-gausian",
+	denoisingNlm = "denoising-nlm",
+	segmentation = "segmentation",
+}
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
+let enumOtions: ButtonOptions;
+
+let dropArea: HTMLElement | null;
+let inputFile: HTMLInputElement | null;
+let imageView: HTMLElement | null;
+let captureImg: HTMLButtonElement | null;
+let divOptions: NodeListOf<HTMLElement> | null;
+let imgDisplay: HTMLImageElement | null;
+
+let image_enhance_btn: HTMLButtonElement | null;
+let image_restoration_btn: HTMLButtonElement | null;
+let morphological_erosion_btn: HTMLButtonElement | null;
+let morphological_dilation_btn: HTMLButtonElement | null;
+let denoising_gausian_btn: HTMLButtonElement | null;
+let denoising_nlm_btn: HTMLButtonElement | null;
+let segmentation_btn: HTMLButtonElement | null;
+
+let imgProcessBtn: HTMLButtonElement | null;
+
 async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+	if (greetMsgEl && greetInputEl) {
+		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+		greetMsgEl.textContent = await invoke("greet", {
+			name: greetInputEl.value,
+		});
+	}
+}
+
+async function imageEnhanceProcess() {
+	const base64String = await convertImgToBase64();
+	const amt: HTMLInputElement | null = document.getElementById(
+		"amt",
+	) as HTMLInputElement;
+	const amt_value = amt ? parseInt(amt.value, 10) : 0;
+	console.log("hola esquizo :u");
+
+	const returnBase64String: String = await invoke("image_enhance", {
+		image_data_base64: base64String,
+		amt: amt_value,
+	});
+
+	// console.log(returnBase64String);
+	if (imgDisplay) {
+		console.log("praise the omnissiah");
+		// imgDisplay.src = base64String;
+		imgDisplay.src = returnBase64String as string;
+		console.log(imgDisplay);
+	}
+}
+
+async function imageRestorationProcess() {
+	const base64String = await convertImgToBase64();
+	const brightness: HTMLInputElement | null = document.getElementById(
+		"brightness",
+	) as HTMLInputElement;
+	const contrast: HTMLInputElement | null = document.getElementById(
+		"contrast",
+	) as HTMLInputElement;
+
+	const brightness_value = brightness ? parseInt(brightness.value, 10) : 0;
+	const contrast_value = contrast ? parseInt(contrast.value, 10) : 0;
+	const returnBase64String: String = await invoke("restore_image", {
+		image_data_base64: base64String,
+		brightness: brightness_value,
+		contrast: contrast_value,
+	});
+
+	if (imgDisplay) imgDisplay.src = returnBase64String as string;
+}
+
+async function morphologicalErosionProcess() {
+	const base64String = await convertImgToBase64();
+	const k_erosion: HTMLInputElement | null = document.getElementById(
+		"k-erosion",
+	) as HTMLInputElement;
+	const k_erosion_value = k_erosion ? parseInt(k_erosion.value, 10) : 0;
+	const returnBase64String: String = await invoke("morphological_erosion", {
+		image_data_base64: base64String,
+		k: k_erosion_value,
+	});
+	if (imgDisplay)
+		imgDisplay.src = `data:image/png;base64,${returnBase64String}`;
+}
+
+async function morphologicalDilationProcess() {
+	const base64String = await convertImgToBase64();
+	const k_dilation: HTMLInputElement | null = document.getElementById(
+		"k-dilation",
+	) as HTMLInputElement;
+	const k_dilation_value = k_dilation ? parseInt(k_dilation.value, 10) : 0;
+	const returnBase64String: String = await invoke("morphological_dilation", {
+		image_data_base64: base64String,
+		k: k_dilation_value,
+	});
+	if (imgDisplay)
+		imgDisplay.src = `data:image/png;base64,${returnBase64String}`;
+}
+
+async function denoisingGausianProcess() {
+	const base64String = await convertImgToBase64();
+	const radius: HTMLInputElement | null = document.getElementById(
+		"radius",
+	) as HTMLInputElement;
+	const radius_value = radius ? parseInt(radius.value, 10) : 0;
+	const returnBase64String: String = await invoke(
+		"denoising_image_gausian_blur",
+		{
+			image_data_base64: base64String,
+			radius: radius_value,
+		},
+	);
+	if (imgDisplay) imgDisplay.src = returnBase64String as string;
+}
+
+async function denoisingNlmProcess() {
+	const base64String = await convertImgToBase64();
+	const window_size: HTMLInputElement | null = document.getElementById(
+		"window_size",
+	) as HTMLInputElement;
+	const h: HTMLInputElement | null = document.getElementById(
+		"h",
+	) as HTMLInputElement;
+	const window_size_value = window_size ? parseInt(window_size.value, 10) : 0;
+	const h_value = h ? parseInt(h.value, 10) : 0;
+	const returnBase64String: String = await invoke("denoising_image_nlm", {
+		image_data_base64: base64String,
+		window_size: window_size_value,
+		h: h_value,
+	});
+	if (imgDisplay)
+		imgDisplay.src = `data:image/png;base64,${returnBase64String}`;
+}
+
+async function segmentationProcess() {
+	const base64String = await convertImgToBase64();
+	const k_segmentation: HTMLInputElement | null = document.getElementById(
+		"k-segmentation",
+	) as HTMLInputElement;
+	const k_segmentation_value = k_segmentation
+		? parseInt(k_segmentation.value, 10)
+		: 0;
+	const returnBase64String: String = await invoke("segment_image", {
+		image_data_base64: base64String,
+		threshold: k_segmentation_value,
+	});
+	if (imgDisplay) imgDisplay.src = returnBase64String as string;
+}
+
+async function convertImgToBase64(): Promise<string> {
+	return new Promise((resolve, reject) => {
+		let file: File | null = null;
+		if (inputFile?.files) file = inputFile?.files[0];
+		const reader = new FileReader();
+		reader.onloadend = function () {
+			if (typeof reader.result === "string") {
+				resolve(reader.result.split(",")[1]);
+			} else {
+				reject("Unexpected result type");
+			}
+		};
+		reader.onerror = reject;
+		if (file) reader.readAsDataURL(file);
+	});
+}
+
+function showDiv(id: String) {
+	if (divOptions)
+		divOptions.forEach((div) => {
+			if (div.id === id) {
+				div.style.display = "block";
+			} else {
+				div.style.display = "none";
+			}
+		});
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+	greetInputEl = document.querySelector("#greet-input");
+	greetMsgEl = document.querySelector("#greet-msg");
+
+	dropArea = document.querySelector("#drop-area");
+	inputFile = document.querySelector("#input-file");
+	imageView = document.querySelector("#img-view");
+	captureImg = document.querySelector("#capture-btn");
+	imgDisplay = document.querySelector("#img-display");
+
+	divOptions = document.querySelectorAll<HTMLElement>("#div-options div");
+
+	image_enhance_btn = document.querySelector("#image-enhance");
+	image_restoration_btn = document.querySelector("#image-restoration");
+	morphological_erosion_btn = document.querySelector("#morphological-erosion");
+	morphological_dilation_btn = document.querySelector(
+		"#morphological-dilation",
+	);
+	denoising_gausian_btn = document.querySelector("#denoising-gausian");
+	denoising_nlm_btn = document.querySelector("#denoising-nlm");
+	segmentation_btn = document.querySelector("#segmentation");
+
+	imgProcessBtn = document.querySelector("#process-image");
+	const uploadImage = () => {
+		if (imageView && inputFile?.files) {
+			const imgLink = URL.createObjectURL(inputFile.files[0]);
+			// console.log(inputFile.files[0]);
+			// console.log(imgLink);
+			// imageView.style.backgroundImage = `url(${imgLink})`;
+			imageView.style.backgroundImage = `url(${imgLink})`;
+			imageView.textContent = " ";
+			imageView.style.border = "0";
+			console.log(imageView);
+			console.log("hola esquizo btw");
+		}
+	};
+
+	image_enhance_btn?.addEventListener("click", () => {
+		showDiv("image-enhance-option");
+		enumOtions = ButtonOptions.imageEnhance;
+	});
+	image_restoration_btn?.addEventListener("click", () => {
+		showDiv("image-restoration-option");
+		enumOtions = ButtonOptions.imageRestoration;
+	});
+	morphological_erosion_btn?.addEventListener("click", () => {
+		showDiv("morphological-erosion-option");
+		enumOtions = ButtonOptions.morphologicalErosion;
+	});
+	morphological_dilation_btn?.addEventListener("click", () => {
+		showDiv("morphological-dilation-option");
+		enumOtions = ButtonOptions.morphologicalDilation;
+	});
+	denoising_gausian_btn?.addEventListener("click", () => {
+		showDiv("denoising-gausian-option");
+		enumOtions = ButtonOptions.denoisingGausian;
+	});
+	denoising_nlm_btn?.addEventListener("click", () => {
+		showDiv("denoising-nlm-option");
+		enumOtions = ButtonOptions.denoisingNlm;
+	});
+	segmentation_btn?.addEventListener("click", () => {
+		showDiv("segmentation-option");
+		enumOtions = ButtonOptions.segmentation;
+	});
+
+	captureImg?.addEventListener("click", () => {});
+
+	inputFile?.addEventListener("change", () => {
+		uploadImage();
+	});
+
+	dropArea?.addEventListener("dragover", (event) => {
+		event.preventDefault();
+	});
+	dropArea?.addEventListener("drop", (event) => {
+		event.preventDefault();
+		console.log("hola esquizo");
+		if (inputFile) {
+			console.log("hola esquizo");
+			const files = event.dataTransfer?.files ?? null;
+			inputFile.files = files;
+			uploadImage();
+		}
+	});
+
+	imgProcessBtn?.addEventListener("click", () => {
+		switch (enumOtions) {
+			case ButtonOptions.imageEnhance:
+				imageEnhanceProcess();
+				break;
+			case ButtonOptions.imageRestoration:
+				imageRestorationProcess();
+				break;
+			case ButtonOptions.morphologicalErosion:
+				morphologicalErosionProcess();
+				break;
+			case ButtonOptions.morphologicalDilation:
+				morphologicalDilationProcess().then();
+				break;
+			case ButtonOptions.denoisingGausian:
+				denoisingGausianProcess();
+				break;
+			case ButtonOptions.denoisingNlm:
+				denoisingNlmProcess();
+				break;
+			case ButtonOptions.segmentation:
+				segmentationProcess();
+				break;
+		}
+	});
+	console.log("hola esquizo");
+	document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+		e.preventDefault();
+		greet();
+	});
 });
